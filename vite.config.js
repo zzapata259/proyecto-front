c6c3dd2f9fb11b50b889fed15e9736b7b084b6dc
zzapata259 <zzapata259@gmail.com>
2026-05-13 23:01:26 -0600

    first committtt

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    allowedHosts: ['proyecto-front-4n14.onrender.com']
+  },
+
+  preview: {
+    allowedHosts: ['proyecto-front-4n14.onrender.com']
+  }
 })
